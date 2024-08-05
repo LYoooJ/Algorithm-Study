0cfd6a11e80c2fb8cd12e07402830823c145edc3
Yoojin Lim <160727704+LYoooJ@users.noreply.github.com>
2024-08-05 23:27:08 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 139.50 ms, Memory: 32.9 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -7,12 +7,8 @@ using namespace std;
 string solution(vector<string> participant, vector<string> completion) {
     map<string, int> m;
     
-    for (vector<string>::iterator iter = participant.begin(); iter != participant.end(); iter++) {
-        if (m.count(*iter) != 0)
+    for (vector<string>::iterator iter = participant.begin(); iter != participant.end(); iter++) 
             m[*iter] += 1;
-        else
-            m[*iter] = 1;
-    }   
     
     for (vector<string>::iterator iter = completion.begin(); iter != completion.end(); iter++) {
         if (m[*iter] == 1)
