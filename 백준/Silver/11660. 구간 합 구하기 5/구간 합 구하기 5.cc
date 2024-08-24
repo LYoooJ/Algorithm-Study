b989883fd328680b778cf64b0ac5d26ddc482db0
Yoojin Lim <160727704+LYoooJ@users.noreply.github.com>
2024-08-24 23:34:02 +0900

    Update 구간 합 구하기 5.cc

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -17,35 +17,16 @@ int main(void)
 
     vector<vector<int>> sum(N + 1, vector<int>(N + 1, 0));
 
-    for (int r = 0; r < N; r++) {
-        for (int c = 0; c < N; c++) {
+    for (int r = 1; r <= N; r++) {
+        for (int c = 1; c <= N; c++) {
             cin >> num;
-            if (r == 0 && c == 0) {
-                sum[r][c] = num;
-            }
-            else if (r == 0 && c != 0) {
-                sum[r][c] = sum[r][c - 1] + num;
-            }
-            else if (r != 0 && c == 0) {
-                sum[r][c] = sum[r - 1][c] + num;
-            }
-            else {
-                sum[r][c] = sum[r - 1][c] + sum[r][c - 1] + num - sum[r - 1][c - 1];
-            }
+            sum[r][c] = sum[r - 1][c] + sum[r][c - 1] + num - sum[r - 1][c - 1];
         }
     }
 
     for (int i = 0; i < M; i++) {
         cin >> r1 >> c1 >> r2 >> c2;
-        if (c1 == 1 && r1 == 1)
-            result = sum[r2 - 1][c2 - 1];
-        else if (c1 == 1 && r1 != 1)
-            result = sum[r2 - 1][c2 - 1] - sum[r1 - 2][c2 - 1];
-        else if (c1 != 1 && r1 == 1)
-            result = sum[r2 - 1][c2 - 1] - sum[r2 - 1][c1 - 2];
-        else
-            result = sum[r2 - 1][c2 - 1] - sum[r1 - 2][c2 - 1] - sum[r2 - 1][c1 - 2] + sum[r1 - 2][c1 - 2];
-
+        result = sum[r2][c2] - sum[r1 - 1][c2] - sum[r2][c1 - 1] + sum[r1 - 1][c1 - 1];
         cout << result << "\n";
     }
 
