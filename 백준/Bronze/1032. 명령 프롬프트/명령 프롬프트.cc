944f62514f788420787cabba50919dff378b728b
Yoojin Lim <160727704+LYoooJ@users.noreply.github.com>
2024-09-06 21:40:46 +0900

    [Bronze I] Title: 명령 프롬프트, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.cc
+++ 백준/Bronze/1032. 명령 프롬프트/명령 프롬프트.cc
@@ -1,42 +1,25 @@
 #include <iostream>
 #include <string>
-#include <list>
 
 using namespace std;
 
 int main(void) {
     int N, size;
-    string name[50];
-    list<int> lt;
-    list<int> diff;
+    string name, cmp;
 
     cin >> N;
-
-    for (int i = 0; i < N; i++) 
-        cin >> name[i];
-    size = name[0].size();
-
-    for (int i = 0; i < size; i++)
-        lt.push_back(i);
+    cin >> name;
+    size = name.size();
 
     for (int i = 1; i < N; i++) {
-        for (list<int>::iterator iter = lt.begin(); iter != lt.end(); ) {
-            int idx = *iter;
-            if (name[i - 1][idx] != name[i][idx]) {
-                iter++;
-                lt.remove(idx);
-                diff.push_back(idx);
-            }
-            else {
-                iter++;
-            }
+        cin >> cmp;
+        for (int j = 0; j < size; j++) {
+            if (name[j] != cmp[j])
+                name[j] = '?';
         }
     }
-    
-    for (list<int>::iterator iter = diff.begin(); iter != diff.end(); iter++) {
-        name[0][*iter] = '?';
-    }
-    cout << name[0];
+
+    cout << name;
 
     return 0;
 }
